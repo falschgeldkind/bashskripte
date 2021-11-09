9b6a1953d71c0fb5c21005c7dde5300a26b32596
Christian Bley <74116031+falschgeldkind@users.noreply.github.com>
2021-11-09 17:01:28 +0100

    Update create-user-config-script.sh

--- user-config/create-user-config-script.sh
+++ user-config/create-user-config-script.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+### Adds a script to autostart on login into GNOME
 
+path_to_script=/PATH/TO/SCRIPT.sh
+script=$(basename $path_to_script)
+
+# Adding directories for autostart files and config scripts 
 mkdir -p /etc/skel/.config/autostart/
 mkdir -p /usr/local/user-config
-cp $path_rsc/user-config.sh /usr/local/user-config/
-chmod +x /usr/local/user-config/user-config.sh
+
+# Copying the script to it's destination and making it executable
+cp $path_to_script /usr/local/user-config/
+chmod +x /usr/local/user-config/$script
 
 cat >/etc/skel/.config/autostart/UCS.desktop <<__EOF__
 [Desktop Entry]
 Type=Application
-Exec=bash /usr/local/user-config/user-config.sh
+Exec=bash /usr/local/user-config/$script
 Hidden=false
 NoDisplay=false
 X-GNOME-Autostart-enabled=true
